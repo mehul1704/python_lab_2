d9a4cec458b7d96338c63c5b07aaa39d7a6e38ff
mehul1704 <bca2022mehul1704@tnraocollege.org>
2024-07-21 23:02:30 +0530

    Add files via upload

---
+++ program_4.py
@@ -0,0 +1,7 @@
+lst=[ 1, 6, 3, 5, 3, 4 ]
+i=7 
+
+if i in lst: 
+    print("exist") 
+else: 
+    print("not exist")
---
+++ program_5.py
@@ -0,0 +1,8 @@
+
+def Reverse(lst):
+   new_lst = lst[::-1]
+   return new_lst
+
+
+lst = [20, 21, 22, 23, 24, 25]
+print(Reverse(lst))
